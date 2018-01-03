6a2c8d5950b2b79bb14ac94641de1c695baeca64
diasaleh <dia-20111@hotmail.com>
2018-01-03 17:16:55 +0200

    split string, convert to int and sum

--- Stringcalc.fs
+++ Stringcalc.fs
@@ -3,13 +3,22 @@ open System
 let splitString (t : string) = 
     let arr = t.Split ','
     arr
-
+let convertSringsListToIntsList (sList:string [])=
+    let len = Array.length sList
+    let intArr = Array.create len 0
+    for i = 0 to len - 1 do
+        intArr.[i] <- sList.[i] |> int
+    intArr
 let Add (s : string) = 
     let st = (s.Trim())
     if not (st.Equals("")) then
         let nums = splitString st
-        printfn "%A" nums 
+        for i in nums do
+            printfn "%A" i
+        let sum =  Array.sum (convertSringsListToIntsList nums)
+        
+        printfn "%A" sum 
     else
         printfn "%s" "0"
     
-Add "   9,9 "
+Add "   9, 10 "
