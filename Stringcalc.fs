9ba927da964ed30dac0ff7ccb4d342521b56fee5
diasaleh <dia-20111@hotmail.com>
2018-01-07 02:22:26 +0200

    7.Delimiters can be of any length
    Delimiters can be of any length with the following format:  “//[delimiter]\n” for example: “//[***]\n1***2***3” should return 6

--- Stringcalc.fs
+++ Stringcalc.fs
@@ -1,41 +1,51 @@
-let splitString (t : string) (delimiter) = 
-    let arr = t.Split delimiter
-    arr
+open System
+let splitString (t : string) (delimiter: string[]) = 
+    t.Split (delimiter, System.StringSplitOptions.None)
+let stringToCharArr (s:string) =
+    [for c in s -> c]|> List.toArray 
+let containsNegativesNums intArr = 
+    intArr |> Array.toList|> List.filter(fun n -> n < 0) |> List.sum 
+let sumArrayIgnoreNumsBiggerThan1000 intArr = 
+    intArr |> Array.toList|> List.filter(fun n -> n <= 1000) |> List.sum   
 let convertSringsListToIntsList (sList:string [])=
     let len = Array.length sList
     let intArr = Array.create len 0
     for i = 0 to len - 1 do
         intArr.[i] <- sList.[i] |> int
-    intArr
-let explode (s:string) =
-    [for c in s -> c]|> List.toArray 
-let containsNegNum intArr = 
-    let sum = intArr |> Array.toList|> List.filter(fun n -> n < 0) |> List.sum
-    sum 
-let sumArrayIgnoreNumsBiggerThan1000 intArr = 
-    let sum = intArr |> Array.toList|> List.filter(fun n -> n <= 1000) |> List.sum
-    sum   
-let setDelimiter (s:string) = 
+    intArr  
+let getFirstNumIndex (s : string)= 
+    let mutable flag = true
+    let mutable index = 0
+    for i in 0 .. s.Length - 1 do
+        if Char.IsNumber s.[i] then 
+            if flag = true then
+                flag <- false
+                index <- i
+    index
+let getNumsString (s:string) = 
     if s.[..1].Equals("//") then
-        let del = explode s.[2..].[..0]
-        let str = s.[4..]
-        del, str
+        let delArr = Array.create 1 ""
+        let delA = [|"["; "]"|]
+        let delString = (splitString s delA).[1]
+        Array.set delArr 0 delString
+        let numsString=(splitString s.[(getFirstNumIndex s)..] delArr)
+        numsString
     else
-        let del = explode "\n |, "
-        del, s
+        let delArr = [|"\n"; ","|]
+        let numsString=(splitString s delArr)
+        numsString
 let Add (s : string) = 
     let st = s.Trim()
     if not (st.Equals("")) then
-        let del, str= setDelimiter st
-        let nums = splitString str del
-        let intAr = convertSringsListToIntsList nums
-        if containsNegNum intAr = 0 then
+        let numsString= getNumsString st
+        printfn "\nSum = %A \n" numsString 
+        let intAr = convertSringsListToIntsList numsString
+        if containsNegativesNums intAr = 0 then
             let sum =  sumArrayIgnoreNumsBiggerThan1000 intAr
-            printfn "%A" sum 
+            printfn "\nSum = %A \n" sum 
         else
-            printfn "negatives not allowed"
-        for i in nums do
-            printfn "%A" i
+            printfn "\nnegatives not allowed\n"
     else
         printfn "%s" "0"
-Add "//;\n2;1001;4;5" 
+Add "//[\n\n\n]\n1\n\n\n2\n\n\n3\n\n\n5"
+Add "//[%%%]\n1%%%2%%%3%%%5"
